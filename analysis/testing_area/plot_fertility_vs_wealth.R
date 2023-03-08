ca3f6df103c3c0e296a9b85e08636681365b31c9
yitalu <luyita.tw@gmail.com>
2023-03-08 11:25:05 +0100

    update

---
+++ analysis/testing_area/plot_fertility_vs_wealth.R
@@ -0,0 +1,36 @@
+# Load Data ----
+source("./analysis/load_data.R")
+d1c0_linked <- fread("./data/testing_area/data_d1c0_linked.csv")
+d1c0_unlinked <- fread("./data/testing_area/data_d1c0_unlinked.csv")
+d1c0_unlinked_0.5 <- fread("./data/testing_area/data_d1c0_unlinked_mutate0.5.csv")
+
+d <- d1c0_linked
+d <- d1c0_unlinked
+d <- d1c0_unlinked_0.5
+colnames(d)[1] <- "inheritance"
+# d <- d1c1
+
+
+
+# Plot Fertility vs Wealth -------------------------------------------------
+# tiff(file = "./figures/fertility_vs_wealth.tiff", width = 2000, height = 1600, res = 300)
+plot(jitter(d$fertility) ~ jitter(d$wealth), ylab = "Fertility", xlab = "Wealth", main = "Wealthier Individuals Give Less Births", pch=20, cex=0.8, col=alpha("#69b3a2", 0.4), cex.main=1, cex.lab=0.9)
+grid()
+# dev.off()
+
+
+
+# Plot Fertility vs Wealth Over Time ---------------------------------------
+# tiff(file = "./figures/fertility_vs_wealth_over_time.tiff", width = 2000, height = 1600, res = 300)
+ggplot(data=d, aes(x=wealth, y=fertility)) +
+  geom_point(color="#69b3a2", alpha=0.4, size=1, position = position_jitter(seed = 1, width = 0.2)) + 
+  theme_ipsum() +
+  facet_wrap(~generation) +
+  theme(
+    # legend.position="none",
+    panel.spacing = unit(0.5, "lines"),
+    axis.ticks.x=element_blank()
+  ) + 
+  labs(title="Fertility vs Wealth Over Time", x ="Wealth", y = "Fertility") + 
+  theme(plot.title = element_text(size = 14, hjust = 0.5))
+# dev.off()
---
+++ analysis/testing_area/plot_strategy_vs_wealth.R
@@ -0,0 +1,30 @@
+# Load Data ----
+source("./analysis/load_data.R")
+d <- d1c0
+# d <- d1c1
+
+
+
+
+# Plot Strategy vs Wealth -------------------------------------------------
+# tiff(file = "./figures/strategy_vs_wealth.tiff", width = 2000, height = 1600, res = 300)
+plot(jitter(d$strategy) ~ jitter(d$wealth), ylab = "Strategy", xlab = "Wealth", main = "Wealthier Individuals Allocate Less to Reproduction", pch=20, cex=0.8, col=alpha("#69b3a2", 0.4), cex.main=1, cex.lab=0.9)
+grid()
+# dev.off()
+
+
+
+# Plot Strategy vs Wealth Over Time ---------------------------------------
+# tiff(file = "./figures/strategy_vs_wealth_over_time.tiff", width = 2000, height = 1600, res = 300)
+ggplot(data=d, aes(x=wealth, y=strategy)) +
+  geom_point(color="#69b3a2", alpha=0.4, size=1, position = position_jitter(seed = 1, width = 0.2)) + 
+  theme_ipsum() +
+  facet_wrap(~generation) +
+  theme(
+    # legend.position="none",
+    panel.spacing = unit(0.5, "lines"),
+    axis.ticks.x=element_blank()
+  ) + 
+  labs(title="Strategy vs Wealth Over Time", x ="Wealth", y = "Strategy") + 
+  theme(plot.title = element_text(size = 14, hjust = 0.5))
+# dev.off()
